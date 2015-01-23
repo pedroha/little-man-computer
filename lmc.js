e208f5821b5d2997c54b26fd22847fb7c02ba211
Pedro Ha <pha@dnainfo.com>
2015-01-23 04:57:26 -0500

    fully implemented and fix dump(), add main(), implement INP/OUT/HLT

--- lmc.js
+++ lmc.js
@@ -1,8 +1,10 @@
 (function() {
     "use strict";
 
-    var inputs = ["INP", "OUT", "HLT"];
+    var USE_UNDEF = true;
+    var MAILBOX_UNDEFINED = (USE_UNDEF) ? '---' : '000';
 
+    // Based on: http://matt.krutar.org/LMC4/
     var LMC = {
         accumulator: 0
       , program_counter: 0
@@ -21,7 +23,7 @@
       LMC.output = 0;
 
       for (var i = 0; i < 100; i++) {
-        LMC.ram[i] = 0;
+        LMC.ram[i] = MAILBOX_UNDEFINED;
       }
     };
 
@@ -33,15 +35,19 @@
             str = "0" + str;
         }
         return str;
-    }
+    };
 
-    var dump = function() {
+    var mailbox = function(val) {
+        return "[" + padZero(val) + "]";
+    };
+
+    var createDump = function() {
         var lines = [];
 
-        lines.push("ACCUMULATOR: " + LMC.accumulator);
-        lines.push("PROGRAM COUNTER: " + LMC.program_counter);
-        lines.push("LAST INPUT: " + LMC.input);
-        lines.push("LAST OUTPUT: " + LMC.output);
+        lines.push("ACCUMULATOR: " + padZero(LMC.accumulator));
+        lines.push("PROGRAM COUNTER: " + padZero(LMC.program_counter));
+        lines.push("LAST INPUT: " + padZero(LMC.input));
+        lines.push("LAST OUTPUT: " + padZero(LMC.output));
 
         var codesPerLine = 4;
 
@@ -53,9 +59,9 @@
         for (var i = 0; i < ram.length; i++) {
             if (i % codesPerLine === 0) {
                 var address = padZero(i) + '-' + padZero((i+codesPerLine-1));
-                ramLine = address + " - " + padZero(ram[i]);
+                ramLine = address + " : ";
             }
-            ramLine = ramLine + " " + padZero(ram[i]);
+            ramLine = ramLine + " " + mailbox(ram[i]);
 
             if (i % codesPerLine === codesPerLine-1) {
                 lines.push(ramLine);
@@ -64,9 +70,40 @@
         return lines;
     };
 
-    reset();
+    var dump = function() {
+        var lines = createDump();
+        var str = lines.join('\n');
+        console.log(str);
+    }
 
-    var core = dump();
+    var load = function(mnemonics, memory) {
+        for (var i = 0; i < mnemonics.length; i++) {
+            var mnemonic = mnemonics[i];
 
-    console.log(core);
+            if (mnemonic === "INP") {
+                memory[i] = "901";
+            }
+            else
+            if (mnemonic === "OUT") {
+                memory[i] = "902";
+            }
+            else
+            if (mnemonic === "HLT") {
+                memory[i] = "000";
+            }
+        }
+    };
+
+    var main = function() {
+        reset();
+        dump();
+
+        var mnemonics = ["INP", "OUT", "HLT"];
+        load(mnemonics, LMC.ram);
+
+        dump();
+    };
+
+    main();
+
 })();
